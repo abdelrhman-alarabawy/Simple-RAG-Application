518bf3eb8be4ac5fde35b64846dbe46af8220744
Abdelrhman Alarabawy <83235049+abdelrhman-alarabawy@users.noreply.github.com>
2025-11-04 20:54:29 +0200

    Enhance file handling and upload functionality: - Add methods for generating unique file paths and cleaning file names in DataController. - Implement async file upload with chunking in data routes. - Update settings to include FILE_DEFAULT_CHUNK_SIZE. - Create ProjectController for managing project directories. - Add .gitignore for assets directory.

--- src/controllers/BaseController.py
+++ src/controllers/BaseController.py
@@ -1,6 +1,13 @@
 from helpers.config import Settings,get_settings
 from fastapi import APIRouter, FastAPI, Depends, UploadFile
-
+import os
+import random
+import string
 class BaseController:
     def __init__(self, app_settings: Settings = Depends(get_settings)):
         self.app_settings = app_settings
+        self.dir = os.path.dirname(os.path.dirname(__file__))
+        self.files_dir = os.path.join(self.dir, "assets/files")
+
+    def generate_random_string(self, length: int=12):
+        return ''.join(random.choices(string.ascii_lowercase + string.digits, k=length))
--- src/routes/data.py
+++ src/routes/data.py
@@ -1,5 +1,13 @@
-from fastapi import APIRouter, FastAPI, Depends, UploadFile
-from controllers import DataController
+import os
+import aiofiles
+from fastapi import APIRouter, FastAPI, Depends, UploadFile, status
+from fastapi.responses import JSONResponse
+from models import ResponseStatus
+import logging
+
+logger = logging.getLogger('uvicorn.error')
+
+from controllers import DataController, ProjectController
 from helpers.config import Settings, get_settings
 data_router = APIRouter(
     prefix="/api/v1/data",
@@ -10,7 +18,44 @@ data_router = APIRouter(
 async def upload_file(project_id: str, file: UploadFile, 
                       app_settings: Settings = Depends(get_settings)):
     #validate file properties
-    is_valid, message = DataController(app_settings).validate_file(file)
+    data_controller = DataController(app_settings)
+    is_valid, message = data_controller.validate_file(file)
     if not is_valid:
-        return {"error": message}
-    return {"message": message}
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST, 
+            content={
+                "error": message
+            }
+        )
+    
+    project_dir_path = ProjectController().get_project_path(project_id)
+
+
+    # file_path = os.path.join(project_dir_path, file.filename)
+    file_path = data_controller.generate_unique_filepath(
+        orig_file_name=file.filename,
+        project_id=project_id
+    )
+    try:
+        async with aiofiles.open(file_path, "wb") as f:
+                    while chunk := await file.read(app_settings.FILE_DEFAULT_CHUNK_SIZE):
+                        await f.write(chunk)
+
+    except Exception as e:
+
+        logger.error(f"File upload failed: {e}")
+
+        return JSONResponse(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            content={
+                "message":ResponseStatus.FILE_UPLOAD_FAILURE
+            }
+        )
+
+    return JSONResponse(
+        content={
+            "message": ResponseStatus.FILE_UPLOAD_SUCCESS,
+        }
+    )
+
+   
