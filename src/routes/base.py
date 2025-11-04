636351dd6b1ad54f77815560e802c23a411faa39
Abdelrhman Alarabawy <83235049+abdelrhman-alarabawy@users.noreply.github.com>
2025-11-04 18:13:33 +0200

    Refactor welcome endpoint to use dependency injection for settings

--- src/routes/base.py
+++ src/routes/base.py
@@ -1,14 +1,13 @@
-from fastapi import APIRouter, FastAPI
+from fastapi import APIRouter, FastAPI, Depends
 import os
-from helpers.config import Settings
+from helpers.config import Settings, get_settings
 base_router = APIRouter(
     prefix="/api/v1",
     tags=["api_v1"],
 )
 
 @base_router.get("/")
-async def welcome():
-    settings = Settings()
+async def welcome(app_settings: Settings = Depends(get_settings)):
     return {
-        "message": f"Welcome to the {settings.APP_NAME} API (v{settings.APP_VERSION})!"
+        "message": f"Welcome to the {app_settings.APP_NAME} API (v{app_settings.APP_VERSION})!"
     }
